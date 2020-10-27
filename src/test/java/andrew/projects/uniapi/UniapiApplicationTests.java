5d451062a8e5c6ac3ba0c86f5f23d9a56a6e146d
Bergmah <52858177+Bergmah@users.noreply.github.com>
2020-10-27 19:43:54 +0200

    Initial commit

---
+++ src/test/java/andrew/projects/uniapi/UniapiApplicationTests.java
@@ -0,0 +1,13 @@
+package andrew.projects.uniapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
